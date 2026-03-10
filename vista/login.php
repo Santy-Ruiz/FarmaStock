f70c7c9222bd42a5ffaae281bf1c1c8ae363e83d
Santy-Ruiz <santiagoruizbam11@gmail.com>
2026-03-10 12:43:38 -0500

    Se sube el proyecto FarmaStok, inicalmente se tiene parte del login y la Base de Datos creada en SQL

---
+++ vista/login.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>login</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" type ref = "text.css" href="../css/login.css/style.css">
+
+</head>
+<body>
+    <img class="wave" src="../img/login.png" alt="Logo">
+    <div class = "contenedor">
+        <div class = "img" >
+            <img src="../img/medicine.svg" alt="Login Image">
+        </div>
+        <div class="contenido-login">
+            <form action="">
+                <img src="...img/doctor.png" alt="">
+                <h2>Farmacia</h2>
+                <div class="input-div Documento Identidad">
+                    <div class="i">
+                        <i class="fas fa-user"></i>
+                    </div>
+                    <div class="div">
+                        <h5>Documento de identidad</h5>
+                        <input type="documento" name = "user" class="input">
+                    </div>
+                </div>
+                <div class="input-div pass">
+                    <div class="i">
+                        <i class="fas fa-lock"></i>
+                    </div>
+                    <div class="div">
+                        <h5>Contraseña</h5>
+                        <input type="Passsword" name = "pass" class="input">
+                    </div>
+                </div>
+                <a href="">Create warpiece</a>
+                <input type="sumbit" class = "btn" value="Iniciar sesion">
+            </form>
+        </div>
+    </div>
+</body>
+</html>

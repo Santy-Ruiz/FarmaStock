caeb1d971b6b046958a8bc863537a4ce754fc2c0
Santy-Ruiz <santiagoruizbam11@gmail.com>
2026-03-10 19:35:49 -0500

    Se agrega el front del login, se agrega codigo js para crear formulario interactivo

--- vista/login.php
+++ vista/login.php
@@ -3,24 +3,24 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>login</title>
+    <title>Login</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" type ref = "text.css" href="../css/login.css/style.css">
+    <link rel="stylesheet" href="../css/style.css">
 
 </head>
 <body>
-    <img class="wave" src="../img/login.png" alt="Logo">
+    <img class="wave" src="../img/login1.png" alt="">
     <div class = "contenedor">
         <div class = "img" >
-            <img src="../img/medicine.svg" alt="Login Image">
+            <img src="../img/medicine.svg" alt="">
         </div>
         <div class="contenido-login">
             <form action="">
-                <img src="...img/doctor.png" alt="">
+                <img src="../img/doctor.png" alt="Doctor">
                 <h2>Farmacia</h2>
-                <div class="input-div Documento Identidad">
+                <div class="input-div documento_identidad">
                     <div class="i">
                         <i class="fas fa-user"></i>
                     </div>
@@ -44,4 +44,5 @@
         </div>
     </div>
 </body>
+<script src="../js/login.js"></script>
 </html>
